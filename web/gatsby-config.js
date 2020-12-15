b65a4451deaed1172a3c206c9a78cbc11fa4f12a
Edd Bush <CC17@edds-mbp.lan>
2020-12-15 14:54:59 +0000

    remove gitignore files

--- web/gatsby-config.js
+++ web/gatsby-config.js
@@ -26,24 +26,6 @@ module.exports = {
           include: /assets/ // See below to configure properly
         }
       }
-    },
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: 'Christopher Gee',
-        short_name: 'Christopher Gee',
-        start_url: '/',
-        background_color: '#000',
-        theme_color: '#000',
-        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
-        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
-        display: 'standalone',
-        icon: 'src/images/favicon.svg', // This path is relative to the root of the site.
-        // An optional attribute which provides support for CORS check.
-        // If you do not provide a crossOrigin option, it will skip CORS for manifest.
-        // Any invalid keyword or empty string defaults to `anonymous`
-        crossOrigin: `use-credentials`
-      }
     }
   ]
 }
