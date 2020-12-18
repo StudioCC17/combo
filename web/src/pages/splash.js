2650d1ff047a0889633876144089f9d0fb599152
StudioCC17 <72558882+StudioCC17@users.noreply.github.com>
2020-12-18 15:39:39 +0000

    Update splash.js

--- web/src/pages/splash.js
+++ web/src/pages/splash.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import Container from '../components/container'
+import Layout from '../containers/layout'
 
 export default function Info() {
   return (
